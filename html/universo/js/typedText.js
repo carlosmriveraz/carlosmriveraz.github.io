189bd6a208f5f281a161d724a4d0285904b7bc9f
carlosmriveraz <Carlosmrz100@hotmail.com>
2024-06-19 02:19:20 -0500

    Add files via upload

---
+++ html/universo/js/typedText.js
@@ -0,0 +1,14 @@
+const options = {
+  strings: [
+    'Fullstack developer',
+    'Android developer',
+    'Dev lover &#128153; &#128155; &#128153; fan',
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true,
+};
+
+const typed = new Typed('.animate-subheading', options);
