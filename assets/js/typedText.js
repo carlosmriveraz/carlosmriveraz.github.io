833cd26ec260d8c5162cba51d0fb87999e6e9e48
carlosmriveraz <Carlosmrz100@hotmail.com>
2024-06-22 23:53:54 -0500

    Add files via upload

--- assets/js/typedText.js
+++ assets/js/typedText.js
@@ -1,14 +1,14 @@
 const options = {
-  strings: [
-    'Fullstack developer',
-    'Android developer',
-    'Dev lover &#128153; &#128155; &#128153; fan',
-  ],
-  typeSpeed: 80,
-  backSpeed: 50,
-  backDelay: 1000,
-  startDelay: 1000,
-  loop: true,
+    strings: [
+        'Desarrollador de Softare',
+        'Ingeniero de Datos',
+        'Arquitecto de Soluciones',
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
 };
 
 const typed = new Typed('.animate-subheading', options);
