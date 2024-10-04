32a56d01e4ad5382c2b626631263cb5d42c1a904
carlosmriveraz <Carlosmrz100@hotmail.com>
2024-10-04 17:49:24 -0500

    Delete assets/apps/sara directory

--- assets/apps/sara/assets/music/reproducir_sonido.js
+++
@@ -1,7 +0,0 @@
-// Crea un nuevo objeto de audio
-const miSonido = new Audio("../../assets/music/juego.mp3");
-
-// Selecciona el elemento que activará el sonido
-document.querySelector(".sonido__hover").addEventListener("mouseover", () => {
-  miSonido.play();
-});
--- assets/apps/sara/js/notificacion__Instrucciones/mensaje_perder.js
+++
@@ -1,8 +0,0 @@
-export function mostrarMensajePerder() {
-  const modal = document.getElementById("modal-perder");
-  modal.style.display = "flex"; // Mostrar el modal de perder
-}
-
-export function cerrarModal(id) {
-  document.getElementById(id).style.display = "none";
-}
