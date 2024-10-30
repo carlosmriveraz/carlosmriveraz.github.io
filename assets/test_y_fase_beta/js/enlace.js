4704aa131a43144467d00d78f0df66c2a434fddc
carlosmriveraz <Carlosmrz100@hotmail.com>
2024-10-29 21:08:32 -0500

    Delete assets/test_y_fase_beta directory

--- assets/test_y_fase_beta/js/enlace.js
+++
@@ -1,8 +0,0 @@
-// Eliminar la clase 'activo' de todos los enlaces
-const enlaces = document.querySelectorAll(".enlace");
-enlaces.forEach((enlace) => {
-  enlace.classList.remove("activo");
-});
-
-// Añadir la clase 'activo' al enlace clicado
-elemento.classList.add("activo");
